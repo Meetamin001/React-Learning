437110c6fe185743b4421d02c5802492a089a5be
Meet Amin <76983731+Meetamin001@users.noreply.github.com>
2022-01-30 00:33:06 -0500

    1st_code

--- reactlearning/src/index.js
+++ reactlearning/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+// var React = require('react'); old verson of javascript that browser understand
+import React from 'react'; // ES6 verson of javascript which browser does not understand, BABEL compile it and convert it into old verson
+// var ReactDOM = require('react-dom');
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <h2> Hello World! </h2>,
-  document.getElementById('root')
-);
+//1st method to display - React with use of JSX
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ReactDOM.render('what to display','where to display','callback func');
+ReactDOM.render(<h1> This is React code HTML Tag called as JSX </h1>, document.getElementById('root'));
+//<h1> tag is not called HTML here, it is called JSX - javaScript Extension or XML
+// you must need 'react' to complile JSX and create elements.
+
+// 2nd method- old version javascript which browser understand
+// ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, " Hello World! "),document.getElementById('root'));
+//--- Babel code conversion
+
+//3rd method - Pure javaScript
+ var h1 = document.createElement('h1');
+ h1.innerHTML = "This is Pure JavaScript";
+document.getElementById("root").appendChild(h1); //--- no BABEL no react, it is pure javaScript
+

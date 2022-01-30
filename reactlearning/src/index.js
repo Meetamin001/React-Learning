a6280dbfb50dc6ca134d0d3643f9848b9bbcb7c9
Meet Amin <76983731+Meetamin001@users.noreply.github.com>
2022-01-30 15:02:48 -0500

    simple_code

--- reactlearning/src/index.js
+++ reactlearning/src/index.js
@@ -1,52 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// if you want to use multiple JSX you have to use <div>
-//because render only accept 1 JSX element
-ReactDOM.render(
-    <div>
-        <h1> with div H1</h1>
-        <p>para</p>
-        <h2>h2 JSX</h2>
-    </div>,
-    document.getElementById("root")
-);
-
-//const arr = ['vinod','abc','dfrc'] array define like this
-//if you don't want to use <div> you can use array too.
-
-ReactDOM.render(
-    [
-        <h1>with array H1</h1>,
-        <p>para</p>,
-        <h2>h2 JSX</h2>
-    ],
-    document.getElementById("root")
-);
-
-// ============== React Frafment ===========
-
-//if you use <div> it will creact another tag inside you div which is on HTML page
-// that's why we use fragment
-ReactDOM.render(
-    <React.Fragment>
-        <h1> with div H1</h1>
-        <p>para</p>
-        <h2>h2 JSX</h2>
-    </React.Fragment>,
-    document.getElementById("root")
-);
-// it is usefull because it use less memory,makes application faster
-
-// Syntactic suger form of Fragment
 ReactDOM.render(
     <>
-        <h1> with div H1</h1>
-        <p>para</p>
-        <h2>h2 JSX</h2>
+        <h1>Netflix serise </h1>
+        <h3>Top 5 Netflix serise</h3>
+        <ul>
+            <li>1st</li>
+            <li>2nd</li>
+            <li>3rd</li>
+            <li>4th</li>
+            <li>5th</li>
+        </ul>
     </>,
     document.getElementById("root")
 );
-// if you just use <> it will be considered as fragment
-
-// ==============End of React Frafment ===========

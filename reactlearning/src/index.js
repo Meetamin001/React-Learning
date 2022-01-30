194e94eb9228dd3e31c7afe891359a34b7b88f9b
Meet Amin <76983731+Meetamin001@users.noreply.github.com>
2022-01-30 17:15:30 -0500

    js_literals

--- reactlearning/src/index.js
+++ reactlearning/src/index.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fname = "firstname"
+const lname = "lastname"
+
 ReactDOM.render(
     <>
-        <h1>Netflix serise </h1>
-        <h3>Top 5 Netflix serise</h3>
-        <ul>
-            <li>1st</li>
-            <li>2nd</li>
-            <li>3rd</li>
-            <li>4th</li>
-            <li>5th</li>
-        </ul>
+    <h3>fname is:  {fname}</h3>
+    <h3>lname is:  {lname}</h3>
+    <h3>fullname is: {fname} {lname}</h3>
+    <h3>concate fname + lname: {fname + " " + lname}</h3>
+    <hr />
+
+    <h2>this is JSX without javascript expression: 2+3</h2>
+    <h2>this is JSX with javascript expression: {2+3}</h2>
+    <hr />
+
+    <h3>random function: {Math.random()}</h3>
+    <hr/>
+
+    <h3>use of <u>Template Literal</u>: {`Firstname is ${fname} and lastname is ${lname}`}</h3>
+    
     </>,
     document.getElementById("root")
 );

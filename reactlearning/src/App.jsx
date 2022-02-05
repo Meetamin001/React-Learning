57d054c3551a68bbf0385ab7a970fefb166e3fca
Meet Amin <76983731+Meetamin001@users.noreply.github.com>
2022-02-04 21:14:10 -0500

    form_data

--- reactlearning/src/App.jsx
+++ reactlearning/src/App.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react/cjs/react.development";
 
 const App = () => {
+    const [fname, setFName] = useState();
+    const [lname, setLName] = useState();
+    const [fullname, setFullName] = useState();
 
-    const [displayData, updateDisplayData] = useState("Click Me!");
-    const [bg, updateBgColor] = useState("blue");
-
-    const cngEvent = () => {
-        let setDisplayData = "Clicked Data";
-        updateDisplayData(setDisplayData);
-
-        let bg = "red";
-        updateBgColor(bg);
+    const input_Fn_Event = (event) => {
+        console.log(event.target.value);
+        setFName(event.target.value);
+    };
+    const input_Ln_Event = (event) => {
+        console.log(event.target.value);
+        setLName(event.target.value);
     };
 
-    const dblClkEvent = () => {
-        let setDisplayData = "Double Clicked Data";
-        updateDisplayData(setDisplayData);
-
-        let bg = "yellow";
-        updateBgColor(bg);
+    const on_Submit = (event) => {
+        event.preventDefault(); // kavascript inbuilt method to data show written in form tag
+        setFullName(fname + " " + lname);
     };
 
     return (
         <>
-            <div style={ { backgroundColor: bg } } >
-                <button onClick={ cngEvent } onDoubleClick={ dblClkEvent }>{ displayData }</button>
+            <div>
+                <form onSubmit={ on_Submit }>
+                    <div>
+                        <h1>Hello, { fullname }</h1>
+                        <input
+                            type="text" placeholder="Enter First Name"
+                            onChange={ input_Fn_Event } //for button we write onClick even same for input onChaange need to be used
+                            value={ fname }  //single source of truth. same variable name use more than 3 times
+                        />
+                        <input
+                            type="text" placeholder="Enter Last Name"
+                            onChange={ input_Ln_Event }
+                            value={ lname }
+                        />
+                        {/* <button onClick={ onSubmit } >Click Me!</button> */ }
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         </>
     );
